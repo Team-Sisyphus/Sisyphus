b53bfcd2905e739e9d97f3c52676dcaaf7646cf0
Ian Sohl <docsohl@gmail.com>
2012-09-29 11:07:27 -0600

    Area of View Binding and Horizontal Movement

--- Assets/Scripts/FirstPersonController.js
+++ Assets/Scripts/FirstPersonController.js
@@ -1,4 +1,5 @@
 var target : Camera;
+var controller : CharacterController;
 //var currentAngle : float = 1;
 var walkSpeed : int = 10;
 var turnSpeed : float = 1;
@@ -12,7 +13,7 @@ function Awake () {
 		target = camera;
 		Debug.Log("Target Automatically Assigned");
 	}
-	var controller : CharacterController = target.collider;
+	controller = target.collider;
 	Debug.Log("Found Collider");
 	//var currentPosition = transform.position;
 }
@@ -24,14 +25,17 @@ function Update () {
 	var x = Input.GetAxis("Mouse X");
 	var y = Input.GetAxis("Mouse Y");
 //	Debug.Log("V: "+v+" - H: "+h+" - X: "+x+" - Y: "+y);
-	Debug.Log(transform.eulerAngles.x);
-	forward = Vector3(0,Mathf.Asin(transform.rotation.x),Mathf.Acos(transform.rotation.x*Mathf.Deg2Rad));
+//	Debug.Log(transform.eulerAngles.x);
+	forward = controller.transform.TransformDirection(Vector3.forward);
+	forward.y = 0;
 //	Debug.Log(forward);
-	transform.Translate(forward*v*walkSpeed*Time.deltaTime,Space.Self);
+	transform.Translate(forward.normalized*v*walkSpeed*Time.deltaTime,Space.World);
 	transform.Translate(Vector3.right*h*walkSpeed/2*Time.deltaTime,Space.Self);
 	transform.Rotate(Vector3.up,x*turnSpeed,Space.World);
-	if (transform.rotation.x > 90 && y != 0){
-		Debug.Log("Oi!");
+	transform.eulerAngles.z=0;
+	Debug.Log(transform.eulerAngles);
+	if (transform.rotation.x > 90){
+		Debug.Log("Rotation > 90");
 	}
 	transform.Rotate(Vector3.right,-y*turnSpeed,Space.Self);
 //	Debug.Log(transform.rotation);
