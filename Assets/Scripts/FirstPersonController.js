58148c4fa50f37e7bbbf80ac507a7e685d5dccb2
Ian Sohl <docsohl@gmail.com>
2012-09-29 09:30:06 -0600

    Mouse Look Movements
    Changing view with mouse 90% complete, I just need to figure out how to
    prevent the view from going past 90 degrees, and implement horizontal
    walking, no matter the angle of view.

--- Assets/Scripts/FirstPersonController.js
+++ Assets/Scripts/FirstPersonController.js
@@ -1,6 +1,6 @@
 var target : Camera;
 //var currentAngle : float = 1;
-var walkSpeed : int = 100;
+var walkSpeed : int = 10;
 var turnSpeed : float = 1;
 
 var smooth = 2.0;
@@ -24,13 +24,20 @@ function Update () {
 	var x = Input.GetAxis("Mouse X");
 	var y = Input.GetAxis("Mouse Y");
 //	Debug.Log("V: "+v+" - H: "+h+" - X: "+x+" - Y: "+y);
-	transform.Translate(Vector3.forward*v*walkSpeed*Time.deltaTime);
-	transform.Translate(Vector3.right*h*walkSpeed/2*Time.deltaTime);
-//	transform.Rotate(y*turnSpeed,x*turnSpeed,0);
+	Debug.Log(transform.eulerAngles.x);
+	forward = Vector3(0,Mathf.Asin(transform.rotation.x),Mathf.Acos(transform.rotation.x*Mathf.Deg2Rad));
+//	Debug.Log(forward);
+	transform.Translate(forward*v*walkSpeed*Time.deltaTime,Space.Self);
+	transform.Translate(Vector3.right*h*walkSpeed/2*Time.deltaTime,Space.Self);
+	transform.Rotate(Vector3.up,x*turnSpeed,Space.World);
+	if (transform.rotation.x > 90 && y != 0){
+		Debug.Log("Oi!");
+	}
+	transform.Rotate(Vector3.right,-y*turnSpeed,Space.Self);
 //	Debug.Log(transform.rotation);
-	var target = Quaternion.Euler (y, 0, x);
-    // Dampen towards the target rotation
-    transform.rotation = Quaternion.Slerp(transform.rotation, target,
-                                   Time.deltaTime * smooth);;
+//	var target = Quaternion.Euler (y, 0, x);
+////     Dampen towards the target rotation
+//    transform.rotation = Quaternion.Slerp(transform.rotation, target,
+//                                   Time.deltaTime * smooth);;
 }
 
